4b8b7baf898f48a6fb6bec9113cf75a7a7487a2a
Shubham <shubhujais1995@gmail.com>
2019-02-06 12:06:19 +0530

    adds basic server setup, adds rental routes, adds DB mongo connection

---
+++ server/models/rental.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const rentalSchema = new schema({
+
+    title : { type: String, required:true, max:[128, 'To long, max charecter is 128']},
+    city : { type: String, required:true, lowercase:true },
+    street : {type: String, required: true, min:[4, 'To short, min 4 characters']},
+    category : { type: String, required: true, lowercase: true },
+    image : { type: String, required: true},
+    bedrooms :Number,
+    shared : Boolean,
+    description : { type: String, required: true},
+    dailyRate : Number,
+    createdAt : { type: Date, default : Date.now }
+    
+});
+
+module.exports = mongoose.model('Rental', rentalSchema);
